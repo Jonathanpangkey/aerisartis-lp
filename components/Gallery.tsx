4e96f9c9ca35769b267f58e094bbef06909f0f92
Jonathan Andreas Pangkey <jonathanpangkey912@gmail.com>
2026-02-04 09:53:54 +0700

    rename portfolio pic name

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -119,19 +119,19 @@ export const Gallery = () => {
     },
     {
       id: 19,
-      image: "/assets/img/portofolio/WhatsApp Image 2026-02-03 at 16.42.46 (1).jpeg",
+      image: "/assets/img/portofolio/portfolio-lampu-robyong-dekorasi-joglo-1.jpeg",
       title: "Lampu Robyong Dekorasi Joglo",
       span: "md:col-span-1 md:row-span-1",
     },
     {
       id: 20,
-      image: "/assets/img/portofolio/WhatsApp Image 2026-02-03 at 16.42.46 (2).jpeg",
+      image: "/assets/img/portofolio/portfolio-lampu-robyong-dekorasi-joglo-2.jpeg",
       title: "Lampu Robyong Dekorasi Joglo",
       span: "md:col-span-1 md:row-span-1",
     },
     {
       id: 21,
-      image: "/assets/img/portofolio/WhatsApp Image 2026-02-03 at 16.42.46.jpeg",
+      image: "/assets/img/portofolio/portfolio-lampu-gantung-hotel-maxone.jpeg",
       title: "Lampu Gantung Hotel MaxOne",
       span: "md:col-span-1 md:row-span-1",
     },
